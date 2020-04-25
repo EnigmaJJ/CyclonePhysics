361f6040224246bcce51aad01824d5b42f99ec98
EnigmaJJ <twj0827@163.com>
2020-04-25 22:38:12 +0800

    初始化工程

---
+++ Assets/CyclonePhysics/Core.cs
@@ -0,0 +1,17 @@
+﻿#if SINGLE_PRECISION
+    using Real = System.Single;
+#else
+    using Real = System.Double;
+#endif
+
+namespace Cyclone
+{
+    public class Vector3
+    {
+        #region Variables
+        public Real x;
+        public Real y;
+        public Real z;
+        #endregion
+    }
+}
