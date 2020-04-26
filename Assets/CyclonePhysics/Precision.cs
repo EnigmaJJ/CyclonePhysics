6e83a2334a2cd4aaa68cd39af740c8340bf1ad03
EnigmaJJ <twj0827@163.com>
2020-04-26 19:50:19 +0800

    新增Vector3结构体 新增RealSqrt函数

--- Assets/CyclonePhysics/Precision.cs
+++ Assets/CyclonePhysics/Precision.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace Cyclone
+{
+    public static class Precision
+    {
+    #if SINGLE_PRECISION
+        public static float RealSqrt(float x)
+        {
+            return (float)System.Math.Sqrt(x);
+        }
+    #else
+        public static double RealSqrt(double x)
+        {
+            return System.Math.Sqrt(x);
+        }
+    #endif
+    }
+}
